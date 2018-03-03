377116558af3b2a7ad743f62ec06610a305b375b
nish996 <p.snishanth996@gmail.com>
2018-03-03 19:32:46 +0530

    updated dup_rem

--- utils/dup_removal.sh
+++ utils/dup_removal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-awk '!seen[$0]++' train_temp.csv > ../dataset/csv_files/train_vrd.csv
-awk '!seen[$0]++' test_temp.csv > ../dataset/csv_files/test_vrd.csv
+awk '!seen[$0]++' train_temp.csv > ../dataset/train_vrd.csv
+awk '!seen[$0]++' test_temp.csv > ../dataset/test_vrd.csv
 rm -rf train_temp.csv
 rm -rf test_temp.csv
